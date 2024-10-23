71ab5b9ab4e5e9d0edf2478f22bc59486379c69f
Leimila <leimilamitei2016@gmail.com>
2024-10-23 23:19:38 +0300

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+//Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;  
+
+// Random
+let random = Math.floor(Math.random() * 90) + 2;  
+
+// Modulus
+let num3 = 36;
+let num4 = 8;
+let mod = num3 % num4;  
+
+// Max
+let max = Math.max(3, 7, 18, 20);  
